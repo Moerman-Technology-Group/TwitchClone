b7e97043c8e178dc194a2ec02e4fb4f88a7210fc
Stephan Moerman <stephan.moerman@viewnary.com>
2023-12-13 18:45:18 +0100

    Added our UI to display users we follow

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,16 +1,20 @@
+import { getFollowedUsers } from "@/lib/follow-service";
+import { getRecommended } from "@/lib/recommended-service";
+
 import { Wrapper } from "./wrapper";
 import { Toggle, ToggleSkeleton } from "./toggle";
+import { Following, FollowingSkeleton } from "./following";
 import { Recommended, RecommendedSkeleton } from "./recommended";
-
-import { getRecommended } from "@/lib/recommended-service";
 
 export const Sidebar = async () => {
   const recommended = await getRecommended();
+  const followedUsers = await getFollowedUsers();
 
   return (
     <Wrapper>
       <Toggle />
       <div className="space-y-4 pt-4 lg:pt-0">
+        <Following data={followedUsers} />
         <Recommended data={recommended} />
       </div>
     </Wrapper>
@@ -21,6 +25,7 @@ export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
       <ToggleSkeleton />
+      <FollowingSkeleton />
       <RecommendedSkeleton />
     </aside>
   );
