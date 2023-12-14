b22d535a9851c1d1a5856c52bc75c8ab12256258
Stephan Moerman <stephan.moerman@viewnary.com>
2023-12-14 13:42:06 +0100

    Added keys UI

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,11 +1,11 @@
 import { getSelf } from "@/lib/auth-service";
-import { getStreamUserById } from "@/lib/stream-service";
+import { getStreamByUserId } from "@/lib/stream-service";
 
 import { ToggleCard } from "./_components/toggle-card";
 
 const ChatPage = async () => {
   const self = await getSelf();
-  const stream = await getStreamUserById(self.id);
+  const stream = await getStreamByUserId(self.id);
 
   if (!stream) {
     throw new Error("Stream not found");
